17b551a59aa4d3fcac06ea9b30001b117b33f8b3
Omar Nasser <omarnasserjr@gmail.com>
2026-07-31 09:59:21 +0300

    refactor: Remove old services
    Signed-off-by: Omar Nasser <omarnasserjr@gmail.com>

--- main.go
+++ main.go
@@ -1,11 +1,4 @@
 package main
 
-import (
-	"github.com/naseyro/minis3/service"
-)
-
 func main() {
-	// change service.Serve() to server.
-	// call the API Service and keep listening
-	service.Serve()
 }
